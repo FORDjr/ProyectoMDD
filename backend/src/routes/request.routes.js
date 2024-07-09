f48a387be68229f848f674193b0af4c8753791af
HunterUrisus <valdebenito.daniel2507@gmail.com>
2024-07-09 00:23:16 -0400

    cambié rutas de request y el getRequestAll

--- backend/src/routes/request.routes.js
+++ backend/src/routes/request.routes.js
@@ -1,28 +1,27 @@
 // request.routes.js
 import { Router } from "express";
-import { createRequest, getRequest, getForms, updateRequest, deleteRequest, acceptRequestController } from "../controllers/request.controller.js";
-import { isAdmin, isManager } from "../middlewares/auth.middleware.js";
+import { createRequest, getRequest, getRequestAll, updateRequest, deleteRequest, acceptRequestController } from "../controllers/request.controller.js";
+import { authorizeRoles } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 // Ruta para crear peticion
 router.post("/", createRequest);
 
-// Ruta para obtener peticion
-router.get("/:id", getRequest);
-
 // Ruta para obtener todas las peticiones
-router.get("/req-all-manager", (isManager), getForms);
-router.get("/req-all-admin", (isAdmin), getForms);
+router.get("/req-all", getRequestAll); //api/request/req-all
+
+// Ruta para obtener peticion
+router.get("/get/:id", getRequest);
 
 // Ruta para modificar peticion
-router.put("/:id", [isAdmin, isManager], updateRequest);
+router.put("/modify/:id", authorizeRoles, updateRequest);
 
 // Ruta para eliminar peticion
-router.delete("/:id", deleteRequest);
+router.delete("/delete/:id", deleteRequest);
 
 //Ruta para aceptar peticiones
-router.put("/:id/accept", [isAdmin, isManager], acceptRequestController);
+router.put("/:id/accept", authorizeRoles, acceptRequestController);
 
 //Actualizar la peticion
 router.get("/:id", )
