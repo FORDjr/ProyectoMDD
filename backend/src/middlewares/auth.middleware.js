70957fe2f07a891a38ac4a8cd20eab3b0ac1e05b
darixdariso <luislagunaspinto@gmail.com>
2024-07-09 00:07:41 -0400

    Actualización de verificación de roles

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,5 +1,5 @@
 /**
- * Middleware para verificar si el usuario es administrador o encargado
+ * Middleware para verificar si el usuario es administrador
  * @param {Object} req - Objeto de petición
  * @param {Object} res - Objeto de respuesta
  * @param {Function} next - Función para continuar con la siguiente función de middleware
@@ -28,21 +28,18 @@ async function isAdmin(req, res, next) {
     return res.status(500).json({ message: 'Error interno del servidor' });
   }
 }
-
-async function isManager(req, res, next) {
-
-  try{
-    //Verifica si hay un usuario autenticado en la sesión
-    if(!req.session.user){
-      return res.status(401).json({message: 'No estás autenticado'});
+async function isEncargado(req, res, next) {
+  try {
+    // Verifica si hay un usuario autenticado en la sesión
+    if (!req.session.user) {
+      return res.status(401).json({ message: 'No estás autenticado' });
     }
-
-    //Obtiene el rol del usuario en la sesión
+    
+    // Obtiene el rol del usuario de la sesión
     const userRole = req.session.user.rolName;
 
-    //Verifica si el usuario tiene el rol de encargado
-    if (userRole === 'encargado'){
-      
+    // Verifica si el usuario tiene el rol de administrador
+    if (userRole === 'encargado') {
       // El usuario tiene el rol adecuado, continua con la siguiente función de middleware
       next();
       return;
@@ -50,12 +47,40 @@ async function isManager(req, res, next) {
       // El usuario no tiene el rol adecuado, devuelve un error de acceso denegado
       return res.status(403).json({ message: 'No tienes permisos para acceder a este recurso' });
     }
-
-  } catch (error){
-    console.log("Error en auth.middleware.js -> isManager(): ", error);
+  } catch (error) {
+    console.log("Error en auth.middleware.js -> isEncargado(): ", error);
     return res.status(500).json({ message: 'Error interno del servidor' });
   }
 }
 
-export { isAdmin };
-export { isManager };
+async function checkRole(req, res, rolesToCheck, next) {
+  try {
+    const userRole = req.session.user.rolName;
+    const hasRole = rolesToCheck.includes(userRole);
+
+    if (hasRole) {
+      return next();
+    }
+
+    return res.status(401).json({
+      message: "No tienes los permisos necesarios para realizar esta acción"
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "authorization.middleware -> checkRole()"
+    });
+  }
+}
+
+function authorizeRoles(...roles) {
+  return (req, res, next) => {
+    const rolesToCheck = roles.map(roleFn => {
+      if (roleFn === isAdmin) return "administrador";
+      if (roleFn === isEncargado) return "encargado";
+      return null;
+    }).filter(Boolean);
+
+    checkRole(req, res, rolesToCheck, next);
+  };
+}
+export { isAdmin, isEncargado, authorizeRoles};
